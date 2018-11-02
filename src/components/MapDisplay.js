151f99ca7cd1547f96c7051ebb7ac9fe00c87105
Ruxandra Vasilescu <ruxilescu@gmail.com>
2018-11-02 15:05:58 -0400

    json error fixed

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -5,8 +5,46 @@ const MAP_KEY = "AIzaSyBMAfzFE8WfGm633IdIuvrRPbtQkuoga4s";
 
 class MapDisplay extends Component {
     
+    state = {
+        map: null
+    };
+
+componentDidMount = () =>{
 }
 
+ mapReady = (props, map) => {
+        // Save the map reference in state and prepare the location markers
+        this.setState({map});
+    }
+
+
+render = () => {
+    const style = {
+        width: "100%",
+        height: "100%"
+    }
+    const center = {
+        lat: this.props.lat,
+        lng: this.props.lng
+    }
+    
+    return (
+        <Map
+        role="application"
+        aria-label="map"
+        onReady={this.mapReady}
+        google={this.props.google}
+        zoom={this.props.zoom}
+        style={style}
+        initialCenter={center}
+        onclick={this.closeInfoWindow}>
+        </Map>
+    
+    )  
+}
+    
+}
+export default GoogleApiWrapper({apiKey: MAP_KEY})(MapDisplay)
 
 
 
@@ -26,18 +64,3 @@ class MapDisplay extends Component {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default MapDisplay;
